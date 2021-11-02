91ce11d6ccbef362144a0412895907496617a0d1
Itamar Carvalho <itamarc@gmail.com>
2021-11-01 21:58:37 -0300

    Finished the code
    Fixed some problems there were on the course.

---
+++ client/src/components/Contacts.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import SingleContact from './SingleContact';
+import AddContact from './AddContact';
+
+export default class Contacts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8080/api/contacts')
+        .then(response => response.json())
+        .then(data => { this.setState({ contacts: data, }) });
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <AddContact />
+                </div>
+                <div className="row">
+                    <h1>contacts</h1>
+                    { this.state.contacts.map(item => ( <SingleContact key={item.id} item={item} /> ))}
+                </div>
+            </div>
+        )
+    }
+}
